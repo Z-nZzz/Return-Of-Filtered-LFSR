a18b44355892bbe3fd2e4750ae6703e1fef799b2
Z/nZ <nn.c@tutanota.com>
2024-07-15 13:19:37 +0200

    replace opti files with correct naive files

--- opti/filter.h
+++ opti/filter.h
@@ -14,7 +14,7 @@ typedef struct {
     uint16_t *taps;
 } Filter;
 
-void filter_init(Filter *filter, uint8_t *key, size_t key_len, uint16_t *P, size_t P_len, int **g, size_t g_len,size_t n, uint16_t *taps);
+void filter_init(Filter *filter, uint8_t *key, size_t key_len, uint16_t *P, size_t P_len, int **g, size_t g_len,size_t n);
 uint8_t filter_bit(Filter *filter);
 uint8_t filter_byte(Filter *filter);
 void free_filter(Filter *filter);
